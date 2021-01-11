4b81870fba61fb01c2f93779793c3e52b2e90528
edxu96 <edxu96@outlook.com>
2021-01-11 11:31:08 +0100

    A case with multiple vertices to be split (#8)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
-"""Configure tests for ``vsec``."""
+"""Configure unit tests in ``vsec``."""
 import networkx as nx
+import pandas as pd
+from pandas.core.frame import DataFrame
 import pytest as pt
+from vsec.geometry import GeoGraph
+
+# Dictionary to map entries in a column.
+VOLTAGES = {
+    "04kv": 0.4,
+    "10kv": 10,
+    "60kv": 60,
+}
 
 
 @pt.fixture(scope="package")
@@ -30,3 +40,37 @@ def case_readme() -> nx.Graph:
     res.add_edge("d", "g", level="low")
     res.add_edge("f", "g", level="low")
     return res
+
+
+@pt.fixture(scope="package")
+def case_grid() -> GeoGraph:
+    """Init a case with 207 edges and **voltage** attributes.
+
+    Note:
+        The dataset used has not been uploaded.
+
+    Returns:
+        A case with 207 edges.
+    """
+    df = pd.read_csv("./data/curves.csv", index_col="name")
+    df["voltage"] = df["voltage"].map(VOLTAGES, na_action="ignore")
+
+    res = nx.from_pandas_edgelist(
+        df, source="from_node", target="to_node", edge_attr=["voltage"]
+    )
+
+    return GeoGraph(res)
+
+
+@pt.fixture(scope="package")
+def vertices_grid() -> DataFrame:
+    """Gather vertices in ``case_grid`` and their **type** attributes.
+
+    Note:
+        The dataset used has not been uploaded.
+
+    Returns:
+        Vertices in ``case_grid`` and their **type** attributes.
+    """
+    df = pd.read_csv("./data/vertices.csv", index_col="name")
+    return df
