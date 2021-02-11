8fd66fb85ce80149fab52de09b1e21430dd7c133
edxu96 <edxu96@outlook.com>
2021-02-11 11:15:44 +0100

    Comment as docstring for attributes in dataclass (#110)

--- mgrid/power_flow/element.py
+++ mgrid/power_flow/element.py
@@ -15,38 +15,31 @@ from dataclasses import dataclass
 class CableEssential:
     """Essential cable parameters."""
 
-    length_km: float
-    """cable length in kilometer"""
+    length_km: float  #: cable length in kilometer
 
 
 @dataclass
 class Cable(CableEssential):
     """Define cable from parameters."""
 
-    r_ohm_per_km: float
-    """resistance in ohm per kilo-meter"""
-    x_ohm_per_km: float
-    """reactance in ohm per kilo-meter"""
-    c_nf_per_km: float
-    """capacitance in nano Farad per kilo-meter"""
-    max_i_ka: float
-    """maximum thermal current in kilo-ampere"""
+    r_ohm_per_km: float  #: resistance in ohm per kilo-meter
+    x_ohm_per_km: float  #: reactance in ohm per kilo-meter
+    c_nf_per_km: float  #: capacitance in nano Farad per kilo-meter
+    max_i_ka: float  #: maximum thermal current in kilo-ampere
 
 
 @dataclass
 class CableStd(CableEssential):
     """Define cable from standard type."""
 
-    std_type: str
-    """a standard cable type"""
+    std_type: str  #: a standard cable type
 
 
 @dataclass
 class TransformerStd:
     """Essential transformer parameters."""
 
-    std_type: str
-    """a standard transformer type"""
+    std_type: str  #: a standard transformer type
 
 
 @dataclass
@@ -60,15 +53,12 @@ class Transformer:
 class Ejection:
     """Essential parameters for load or static generators."""
 
-    p_mw: float
-    """ejected real power in mega-watt"""
-    q_mvar: float
-    """ejected reactive power in mega-watt"""
+    p_mw: float  #: ejected real power in mega-watt
+    q_mvar: float  #: ejected reactive power in mega-watt
 
 
 @dataclass
 class ExternalGrid:
     """Define a slack bus and a corresponding external grid."""
 
-    vm_pu: float
-    """voltage magnitude in per unit"""
+    vm_pu: float  #: voltage magnitude in per unit
